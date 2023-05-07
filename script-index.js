79b7f08d3e8265263ae4d0268cef92c5d4706f6e
Cami <camiladuartebrovia@gmail.com>
2023-05-07 01:16:32 -0300

    repo hospi 2

---
+++ script-index.js
@@ -0,0 +1,17 @@
+function menuLink(link) {
+    console.log(link.value);
+    window.open(link.value)
+};
+
+document.getElementById("btn").addEventListener("click",e=>{
+    window.location.href = "menu.html"
+});
+
+function redireccion(){
+    localtion.href = "menu.html"
+}
+
+function entrar(){
+    window.location.href = "menu.html";
+}
+      
